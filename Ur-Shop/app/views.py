dcd1f5fd64a2e7f10e18add90c84888a6c4ab533
Hacnine <hacninetusar@gmail.com>
2022-03-16 23:16:42 +0600

    'Ur-Shop'

--- Ur-Shop/app/views.py
+++ Ur-Shop/app/views.py
@@ -129,9 +129,9 @@ def address(request):
 def add_to_cart(request):
     usr = request.user
     product_id = request.GET.get('prod_id')
-    print('id', product_id)
+    # print('id', product_id)
     product = Product.objects.get(pk=product_id)
-    print('instance', product)
+    # print('instance', product)
     Cart(user=usr, product=product).save()
 
     return redirect('/cart')
@@ -141,6 +141,23 @@ def show_cart(request):
     if request.user.is_authenticated:
         user = request.user
         carts = Cart.objects.filter(user=user)
-        return render(request, 'app/addtocart.html', {'carts': carts})
+        print(carts)
+
+        cart_product = [p for p in carts]
+        print(cart_product)
+
+        amount = 0.0
+        shipping_amount = 70.0
+        total_amount = 0.0
+        # cart_product = [p for p in Cart.objects.all() if p.user == user]
+        # print(cart_product)
+        if cart_product:
+            for p in cart_product:
+                temp_amount = (p.quantity * p.product.discounted_price)
+                amount += temp_amount
+                total_amount = amount + shipping_amount
+                print(p.id)
+
+        return render(request, 'app/addtocart.html', {'carts': carts, 'amount': amount, 'total_amount': total_amount})
 
 
